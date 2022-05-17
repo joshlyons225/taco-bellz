26e6735b74ab7d12cf89fcc1d62653e1e436fa1d
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-16 23:13:11 -0500

    added navbar test components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   const clickHandler = () => setNav(!nav);
 
   return (
-    <section name="navbar" className="w-full h-auto">
-      <div className="w-full flex flex-column p-4">
+    <section name="navbar" className="w-full h-full bg-teal-200">
+      <div className="w-full flex flex-column p-2">
         {/* Nav burger */}
         <div onClick={clickHandler} className="hidden z-10">
           {!nav ? <FaBars /> : <FaTimes />}
@@ -21,9 +21,18 @@ const Navbar = () => {
               : "absolute top-0 left-0 w-full h-screen flex flex-col"
           }
         >
-          <li className="py-6 text-4xl">
+          <li className="py-4 text-4xl text-purple-900">
             {" "}
             <Link onClick={clickHandler} to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+        </ul>
+
+        {/* Navbar menu */}
+        <ul className="hidden md:flex py-4 text-2xl text-bold text-purple-900">
+          <li>
+            <Link to="home" smooth={true} duration={500}>
               Home
             </Link>
           </li>
