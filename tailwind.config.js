4a07829a74b8f458b95ea966ca0d47810a6e6c6f
TennCreekBridges <bridgettvalet@gmail.com>
2022-05-17 10:23:51 -0500

    adds tailwind output.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    fontFamily: {
-      sans: ['"Josefin Sans"'],
-      body: ['"Josefin Sans"'],
-    },
+    // fontFamily: {
+    //   sans: ['"Josefin Sans"', ...defaultTheme.fontFamily.sans],
+    //   body: ['"Josefin Sans"'],
+    // },
     extend: {},
   },
   plugins: [],
