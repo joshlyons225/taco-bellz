fcd33fa9944c4e004e31dbde99ac865f811f1444
TennCreekBridges <bridgettvalet@gmail.com>
2022-05-17 14:34:05 -0500

    tailwind self-teaching tutorial time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    // fontFamily: {
-    //   sans: ['"Josefin Sans"', ...defaultTheme.fontFamily.sans],
-    //   body: ['"Josefin Sans"'],
-    // },
+    fontFamily: {
+      sans: ['"Josefin Sans"', '...defaultTheme.fontFamily.sans'],
+      body: ['"Josefin Sans"', '...defaultTheme.fontFamily.sans'],
+      p: ['"Josefin Sans"', '...defaultTheme.fontFamily.sans'],
+    },
     extend: {},
   },
   plugins: [],
