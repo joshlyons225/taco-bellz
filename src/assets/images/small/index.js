77c559b64faea1e7821199ba84c481eb2d0b651c
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-18 09:56:32 -0500

    added index file to small images folder and exported all images as one pack to component

---
+++ src/assets/images/small/index.js
@@ -0,0 +1,31 @@
+// import all img files
+import BBChalupa from "./black_bean_chalupa_640x650.jpg";
+import BBQuesarito from "./black_bean_quesarito_640x650.jpg";
+import CBRBurrito from "./cheesy_bean_and_rice_burrito_640x650.jpg";
+import GorditaCrunch from "./cheesy_gordita_crunch_640x650.jpg";
+import ChikChalupa from "./chicken_chalupa_supreme_640x650.jpg";
+import MexPizza from "./mexican_pizza_640x650.jpg";
+import DoritosLocos from "./nacho_cheese_doritos_locos_tacos_supreme_640x650.jpg";
+import BellGrande from "./nachos_bellgrande_640x650.jpg";
+import ChikQuesadilla from "./quesadilla_640x650.jpg";
+import SoftTaco from "./soft_taco_640x650.jpg";
+import PotTaco from "./spicy_potato_soft_taco_640x650.jpg";
+import VegCrunchWrap from "./vegetarian_crunchwrap_640x650.jpg";
+import RanchFryBurr from "./white_hot_ranch_burrito_-_veggie_640x650.jpg";
+
+// export img pack
+export {
+  BBChalupa,
+  BBQuesarito,
+  CBRBurrito,
+  GorditaCrunch,
+  ChikChalupa,
+  MexPizza,
+  DoritosLocos,
+  BellGrande,
+  ChikQuesadilla,
+  SoftTaco,
+  PotTaco,
+  VegCrunchWrap,
+  RanchFryBurr,
+};
--- src/components/Throwdown.jsx
+++ src/components/Throwdown.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import BBChalupa from "../assets/images/small/black_bean_chalupa_640x650.jpg";
-import BBQuesarito from "../assets/images/small/black_bean_quesarito_640x650.jpg";
-import CBRBurrito from "../assets/images/small/cheesy_bean_and_rice_burrito_640x650.jpg";
-import GorditaCrunch from "../assets/images/small/cheesy_gordita_crunch_640x650.jpg";
-import ChikChalupa from "../assets/images/small/chicken_chalupa_supreme_640x650.jpg";
-import MexPizza from "../assets/images/small/mexican_pizza_640x650.jpg";
-import DoritosLocos from "../assets/images/small/nacho_cheese_doritos_locos_tacos_supreme_640x650.jpg";
-import BellGrande from "../assets/images/small/nachos_bellgrande_640x650.jpg";
-import ChikQuesadilla from "../assets/images/small/quesadilla_640x650.jpg";
-import SoftTaco from "../assets/images/small/soft_taco_640x650.jpg";
-import PotTaco from "../assets/images/small/spicy_potato_soft_taco_640x650.jpg";
-import VegCrunchWrap from "../assets/images/small/vegetarian_crunchwrap_640x650.jpg";
-import RanchFryBurr from "../assets/images/small/white_hot_ranch_burrito_-_veggie_640x650.jpg";
+import {
+  BBChalupa,
+  BBQuesarito,
+  CBRBurrito,
+  GorditaCrunch,
+  ChikChalupa,
+  MexPizza,
+  DoritosLocos,
+  BellGrande,
+  ChikQuesadilla,
+  SoftTaco,
+  PotTaco,
+  VegCrunchWrap,
+  RanchFryBurr,
+} from "../assets/images/small/index";
 
 const Throwdown = () => {
   return (
