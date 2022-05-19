be40e61475fa407e124b483475a517ce1baa2120
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-18 21:24:27 -0500

    made footer icons larger

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,12 @@ const Footer = () => {
   return (
     <footer className="w-full h-auto bg-black">
       <h1 className="text-white text-center py-2">
-        <FaToilet /> Insert Our Names with GitHub Links Here
+        <FaToilet size={30} color="white" /> Insert Our Names with GitHub Links
+        Here
       </h1>
       <h2 className="text-white text-center py-2">
-        <FaToiletPaper /> &copy; 2022 Top Shelf Productions
+        <FaToiletPaper size={30} color="white" /> &copy; 2022 Top Shelf
+        Productions
       </h2>
     </footer>
   );
