1bfb92b853e71c6b397f2bf0935e4f860e9d2e42
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-18 21:46:04 -0500

    fixed footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,14 @@ import { FaToilet, FaToiletPaper } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="w-full h-auto bg-black">
-      <h1 className="text-white text-center py-2">
+    <footer className="w-full h-auto flex justify-center bg-black">
+      <h1 className="text-white text-center inline-flex py-2">
         <FaToilet size={30} color="white" /> Insert Our Names with GitHub Links
         Here
+        <br></br>
+        &copy; 2022 Top Shelf Productions
+        <FaToiletPaper size={30} color="white" />
       </h1>
-      <h2 className="text-white text-center py-2">
-        <FaToiletPaper size={30} color="white" /> &copy; 2022 Top Shelf
-        Productions
-      </h2>
     </footer>
   );
 };
