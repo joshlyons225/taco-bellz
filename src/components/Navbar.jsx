ec44b5f78a47ed897e91af7d186b6ba6c35cbb62
TennCreekBridges <bridgettvalet@gmail.com>
2022-05-18 22:54:49 -0500

    adds fade to nav and adds logo component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { Link } from "react-scroll";
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link } from 'react-scroll';
 
 // create Navbar section function
 const Navbar = () => {
@@ -8,45 +8,48 @@ const Navbar = () => {
   const clickHandler = () => setNav(!nav);
 
   return (
-    <section name="navbar" className="w-full max-h-28 bg-teal-200 sticky">
-      <div className="w-full grid grid-cols-2 flex-wrap justify-items-between p-2">
-        <div className="invisible">
+    <section
+      name='navbar'
+      className='w-full h-64 bg-gradient-to-b from-purple-800  to-red-200 sticky'
+    >
+      <div className='w-full grid grid-cols-2 flex-wrap justify-items-between p-2'>
+        <div className='invisible'>
           {/* Navbar menu */}
-          <ul className="flex md:visible py-4 text-2xl text-bold text-purple-900">
+          <ul className='flex md:visible py-4 text-2xl text-bold text-purple-900'>
             <li>
-              <Link to="home" smooth={true} duration={500}>
-                Home
+              <Link to='home' smooth={true} duration={500}>
+                HOME
               </Link>
             </li>
             <li>
-              <Link to="throwdown" smooth={true} duration={500}>
-                Throwdown
+              <Link to='throwdown' smooth={true} duration={500}>
+                THROWDOWN
               </Link>
             </li>
           </ul>
         </div>
         {/* Nav burger */}
-        <div onClick={clickHandler} className="md:invisible z-10">
+        <div onClick={clickHandler} className='md:invisible z-10'>
           {!nav ? <FaBars /> : <FaTimes />}
         </div>
         <ul
           className={
             !nav
-              ? "invisible"
-              : "sticky top-0 left-0 w-full h-full flex flex-col"
+              ? 'invisible'
+              : 'sticky top-0 left-0 w-full h-full flex flex-col'
           }
         >
-          <li className="py-4 text-4xl text-purple-900">
-            {" "}
-            <Link onClick={clickHandler} to="home" smooth={true} duration={500}>
+          <li className='py-4 text-4xl text-purple-900'>
+            {' '}
+            <Link onClick={clickHandler} to='home' smooth={true} duration={500}>
               Home
             </Link>
           </li>
-          <li className="py-4 text-4xl text-purple-900">
-            {" "}
+          <li className='py-4 text-4xl text-purple-900'>
+            {' '}
             <Link
               onClick={clickHandler}
-              to="throwdown"
+              to='throwdown'
               smooth={true}
               duration={500}
             >
