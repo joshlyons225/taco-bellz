2cffb9b4525228d117e8da87452aa6dd2df6a4c2
TennCreekBridges <bridgettvalet@gmail.com>
2022-05-18 21:07:56 -0500

    pushes bv rubbish

--- src/components/Navbarv.jsx
+++ src/components/Navbarv.jsx
@@ -1,92 +1,49 @@
-import { useState } from 'react'; // import state
+//SlideOutMenuContainer.js
+import React, { Component } from 'react';
+import './App.css';
 
-export default function Navbarv() {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-  // initiate isNavOpen state with false
+class Navbarv extends Component {
+  constructor(props, context) {
+    super(props, context);
 
-  return (
-    <div className='flex items-center justify-between border-b border-gray-400 py-8'>
-      <a href='/'>
-        <img src='../assets/images/nav-logo.png' alt='logo' />
-      </a>
-      <nav>
-        <section className='MOBILE-MENU flex lg:hidden'>
-          <div
-            className='HAMBURGER-ICON space-y-2'
-            onClick={() => setIsNavOpen((prev) => !prev)}
-            // toggle isNavOpen state on click
+    this.state = {
+      visible: false,
+    };
+  }
+  render() {
+    const { visible } = this.state;
+
+    return (
+      <div>
+        <div id='slide_nav'>
+          <p
+            id='slide_nav_button'
+            onClick={() => this.setState({ visible: !visible })}
           >
-            <span className='block h-0.5 w-8 animate-pulse bg-gray-600'></span>
-            <span className='block h-0.5 w-8 animate-pulse bg-gray-600'></span>
-            <span className='block h-0.5 w-8 animate-pulse bg-gray-600'></span>
-          </div>
-
-          <div className={isNavOpen ? 'showMenuNav' : 'hideMenuNav'}>
-            {' '}
-            {/* toggle class based on isNavOpen state */}
-            <div
-              className='CROSS-ICON absolute top-0 right-0 px-8 py-8'
-              onClick={() => setIsNavOpen(false)}
-              // change isNavOpen state to false to close the menu
-            >
-              <svg
-                className='h-8 w-8 text-gray-600'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              >
-                <line x1='18' y1='6' x2='6' y2='18' />
-                <line x1='6' y1='6' x2='18' y2='18' />
-              </svg>
-            </div>
-            <ul className='MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]'>
-              <li className='border-b border-gray-400 my-8 uppercase'>
-                <a href='/about'>About</a>
-              </li>
-              <li className='border-b border-gray-400 my-8 uppercase'>
-                <a href='/portfolio'>Portfolio</a>
-              </li>
-              <li className='border-b border-gray-400 my-8 uppercase'>
-                <a href='/contact'>Contact</a>
-              </li>
-            </ul>
-          </div>
-        </section>
-
-        <ul className='DESKTOP-MENU hidden space-x-8 lg:flex'>
-          <li>
-            <a href='/about'>About</a>
-          </li>
-          <li>
-            <a href='/portfolio'>Portfolio</a>
-          </li>
-          <li>
-            <a href='/contact'>Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        background: white;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-    `}</style>
-    </div>
-  );
+            ☰
+          </p>
+        </div>
+        <div>
+          <ul id='slide_menu' style={{ display: visible ? 'block' : 'none' }}>
+            <li>
+              <a href='#'>Home</a>
+            </li>
+            <li>
+              <a href='#'>About</a>
+            </li>
+            <li>
+              <a href='#'>Contact</a>
+            </li>
+            <li>
+              <a href='#'>Tutorial</a>
+            </li>
+            <li>
+              <a href='#'>Advertise</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
+export default Navbarv;
