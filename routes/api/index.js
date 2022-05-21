2a790c53c80184a6e8af05b9c88351a207aa902d
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-20 21:42:35 -0500

    added file structure for routes directory

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,11 @@
 // set dependencies
 const router = require("express").Router();
+const commentRoutes = require("./comment-routes");
+const userRoutes = require("./user-routes");
+
+// set api endpoints
+router.use("/comments", commentRoutes);
+router.use("/users", userRoutes);
 
 // export api routes
 module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+
+// export this route to index
+module.exports = router;
