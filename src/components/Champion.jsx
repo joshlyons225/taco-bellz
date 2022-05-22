0fcdfec826e5e956cb647b7325ab8ca4f45809f1
TennCreekBridges <bridgettvalet@gmail.com>
2022-05-22 14:50:40 -0500

    fixes package.json error i made and adds campeon section

--- src/components/Champion.jsx
+++ src/components/Champion.jsx
@@ -1,16 +1,30 @@
 // import files and dependencies
 import React from 'react';
+import Campeon from '../assets/images/campeon.png';
 
 const Champion = () => {
   return (
-    <section name='champ' className='w-full h-auto'>
-      <div className='bg-pink-600 py-4 px-4'>
-        <p className='py-2 text-white'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus
-          commodo viverra maecenas accumsan lacus. Tincidunt nunc pulvinar
-          sapien et ligula ullamcorper malesuada.
-        </p>
+    <section name='champ' className='bg-red-800 pb-5'>
+      <div className=' items-center md:flex'>
+        <div className='flex-auto mx-auto p-4'>
+          <h1 className='text-violet-900 text-7xl py-4  sm:text-center md:text-left'>
+            WEEKLY CHAMPION
+          </h1>
+          <p className='text-violet-900 pr-6 sm:text-center md:text-left'>
+            In life, there are winners, and there are losers. Here we present
+            the cherished Taco Bell menu item that rules them all (if you spent
+            all day mashing the UPVOTE button, good for you. We appreciate that
+            kind of dedication, and your spot at the top is well-earned).
+          </p>
+        </div>
+
+        <div className='p-4 h-auto sm:flex-col'>
+          <img
+            src={Campeon}
+            alt='Luchador Chiuahua with the words Taco Bell Campeon'
+            className='mx-auto h-full'
+          ></img>
+        </div>
       </div>
     </section>
   );
