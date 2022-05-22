050b2f0823d9f2aa3ad5e4e6a461a381cb1fa42c
TennCreekBridges <bridgettvalet@gmail.com>
2022-05-21 22:46:36 -0500

    adds Champion.jsx section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,10 @@
 // import files and dependencies
 import React from 'react';
 import Logo from '../assets/images/Yo-Quiero.png';
-import Tacoz from '../assets/Tacoz.jpg';
 
 const Home = () => {
   return (
-    <section name='home' className='bg-red-200'>
+    <section name='home' className='bg-red-200 pb-5'>
       <div className=' items-center md:flex'>
         <div className='p-4 h-auto sm:flex-col'>
           <img src={Logo} alt='Yo Quiero Logo' className='mx-auto h-full'></img>
@@ -25,43 +24,6 @@ const Home = () => {
             go head-to-head. Cause we're down for some combat, especially when
             it comes to these contenders.
           </p>
-        </div>
-      </div>
-      {/* CLASS TEST */}
-
-      <div className='bg-pink-600 py-4 px-4'>
-        <p className='py-2 text-white'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus
-          commodo viverra maecenas accumsan lacus. Tincidunt nunc pulvinar
-          sapien et ligula ullamcorper malesuada.
-        </p>
-
-        {/* BUTTON TEST */}
-
-        <button className='bg-pink-300 hover:bg-pink-100 text-black font-bold py-2 px-4 rounded'>
-          UPVOTE
-        </button>
-
-        {/* CARD COMPONENT */}
-        <div className='max-w-sm mx-auto flex p-6 m-5 bg-white rounded-lg shadow-xl'>
-          <div className='flex shrink 0'>
-            <img src={Tacoz} alt='glory taco' className='h-12 w-12'></img>
-          </div>
-          <div className='ml-6 pt-1'>
-            <h4 className='text-xl text-gray-900'>DREAMY</h4>
-            <p className='text-base text-gray-600'>Look Here.</p>
-          </div>
-        </div>
-
-        <div className='max-w-sm mx-auto flex p-6 m-5 bg-white rounded-lg shadow-xl'>
-          <div className='flex shrink 0'>
-            <img src={Tacoz} alt='glory taco' className='h-12 w-12'></img>
-          </div>
-          <div className='ml-6 pt-1'>
-            <h4 className='text-xl text-gray-900'>DREAMY</h4>
-            <p className='text-base text-gray-600'>Look Here.</p>
-          </div>
         </div>
       </div>
     </section>
