7782cc94f19b5f6e9b38d0e683322bc8ce2b6bbe
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-22 04:03:42 -0500

    added all comment and reply routes to comment-routes.js

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,4 +1,20 @@
 const router = require("express").Router();
 
+const {
+  addComment,
+  addReply,
+  removeComment,
+  removeReply,
+} = require("../../controllers/comment-controller");
+
+// add comment to menu item
+router.route("/:menuId").post(addComment);
+
+// add reply and delete comment
+router.route("/menuId/:commentId").put(addReply).delete(removeComment);
+
+// delete reply
+router.route("/menuId/:commentId/:replyId").delete(removeReply);
+
 // export this route to index
 module.exports = router;
