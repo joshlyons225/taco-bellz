a8b1fbbe2b7c1bcf428e5876fd90c9d02488d507
TennCreekBridges <bridgettvalet@gmail.com>
2022-05-22 17:27:53 -0500

    working on champion section info

--- src/components/Champion.jsx
+++ src/components/Champion.jsx
@@ -36,16 +36,14 @@ const Champion = () => {
             className='mx-auto max-w-full rounded-md shadow-lg'
           ></img>
 
-          <div>
-            <p className='text-6xl text-rose-500 champ'>
+          <div className='w-1/2 absolute top-10 right-10'>
+            <p className='text-6xl text-rose-500 champ w-1/2'>
               BLACK BEAN CHALUPA SUPREME
             </p>
-            <p className='text-rose-500 absolute top-20 right-5 p-4 sm:text-center md:text-left'>
-              In life, there are winners, and there are losers. Here we present
-              the cherished Taco Bell menu item that rules them all (if you
-              spent all day mashing the UPVOTE button, good for you. We
-              appreciate that kind of dedication, and your spot at the top is
-              well-earned).
+            <p className='text-rose-500 absolute right-12 pt-1 pl-3 sm:text-center md:text-left w-1/2'>
+              And right here, we have the winner. You know. The kind of thing
+              that you keep going back to time and again, although you know it
+              just might be a toxic relationship. We're okay with that, too.
             </p>
           </div>
         </div>
